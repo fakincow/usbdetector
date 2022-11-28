0a407db28fac593c809aa1e494cfdb7f7814c9db
fakincow <zabaroth@gmail.com>
2022-11-28 15:32:05 +0200

    prompt

--- mcetask/client/ReactClient/wslistener/src/components/InputNavigator.tsx
+++ mcetask/client/ReactClient/wslistener/src/components/InputNavigator.tsx
@@ -3,10 +3,13 @@ import TreeView from '@mui/lab/TreeView';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import TreeItem from '@mui/lab/TreeItem';
+import { Modal } from "antd";
 
 export function FileSystemNavigator({ tree }) {
   const [mainCategory, setMainCategory] = React.useState([]);
-
+  const [showPrompt, setShowPrompt] = React.useState(false);
+  const [selectedItemLabel, setSelectedItemLabel] = React.useState();
+  const [selectedItemDescription, setSelectedItemDescription] = React.useState();
   let initNumber = 0;
   React.useEffect(() => {
     let newMenu = [];
@@ -19,24 +22,48 @@ export function FileSystemNavigator({ tree }) {
     }
     setMainCategory(newMenu)
   }, [tree]);
+  const onSelect = (e) => {
+    console.log('onselect',e)
+    setSelectedItemLabel(e.deviceName);
+    setShowPrompt(true)
+
+  }
+  const handleOK = () => {
+    setShowPrompt(false)
+
+  }
+  const handleCancel = () => {
+    setShowPrompt(false)
+  }
   return (
-    <div>
+    <div className="bg-gray-100 w-full">
       <TreeView
         aria-label="file system navigator"
         defaultCollapseIcon={<ExpandMoreIcon />}
         defaultExpandIcon={<ChevronRightIcon />}
-        sx={{ height: 340, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+        sx={{ height:440, flexGrow: 1, maxWidth: 500, overflowY: 'auto' }}
       >
         {mainCategory.map((e, i) => {
           initNumber++
           return <TreeItem nodeId={initNumber.toString()} label={e.name} key={i}>
             {e.children.map((e, i) => {
               initNumber++
-              return <TreeItem nodeId={initNumber.toString()} label={e['productId']} key={i} />
+              return <TreeItem nodeId={initNumber.toString()} label={e['productId']} key={i} onClick={()=> onSelect(e)} />
             })}
           </TreeItem>
         })}
       </TreeView>
+      <Modal
+      title={selectedItemLabel}
+      visible={showPrompt}
+      onOk={handleOK}
+      onCancel={handleOK}
+      okText="CLOSE"
+      cancelText="CLOSE"
+      closable={true}
+    >
+      SELECTED ITEM DETAILS
+    </Modal>
     </div>
   );
 }
--- mcetask/client/ReactClient/wslistener/src/pages/hubConnection.tsx
+++ mcetask/client/ReactClient/wslistener/src/pages/hubConnection.tsx
@@ -1,23 +1,11 @@
 
 import io from 'socket.io-client';
 import { useEffect, useState } from "react";
-import { Manager } from "socket.io-client";
-import { w3cwebsocket as W3CWebSocket } from "websocket";
 import { DeviceCard } from "../components/DeviceCard";
 import { FileSystemNavigator } from "../components/InputNavigator";
-import { ITreeCategory } from "../models/models";
-var parser = require('socket.io-parser');
-var encoder = new parser.Encoder();
-var packet = {
-    type: parser.EVENT,
-    data: 'test-packet',
-    id: 13
-};
-const manager = new Manager("http://localhost:9000", {
-    autoConnect: false
-});
-//const socket = manager.socket("/");
-const socket = io("http://localhost:9000");
+
+const connectionAdress: string = "http://localhost:9000";
+const socket = io(connectionAdress);
 export function HubConnection() {
     const [isConnected, setIsConnected] = useState(socket.connected);
     const [lastPong, setLastPong] = useState('');
@@ -62,13 +50,6 @@ export function HubConnection() {
         });
     }
     useEffect(() => {
-        manager.open((err) => {
-            if (err) {
-                console.log('an error has occurred'); // an error has occurred
-            } else {
-                console.log('connection was successfully established');// the connection was successfully established
-            }
-        });
         initSocket()
 
         //return () => {
@@ -79,21 +60,19 @@ export function HubConnection() {
     }, []);
 
     return (
-        <div className='flex justify-center pt-10 mx-auto h-scrren w-screen'>
-
-            <div className="bg-gray-100 w-4/6">
-                <ul>
-                    <p className="text-lg font-bold bg-green-100">Connected: {'' + isConnected}</p>
-                    {devices.map((item, index) => {
-                        return <DeviceCard device={item} key={index} />
-                    })}
-                </ul>
-            </div>
-            <div className="bg-gray-200 w-2/6">
-                <ul>
-                    tree of devices
+        <div>
+            <p className="text-lg font-bold bg-green-100">Connected: {'' + isConnected}</p>
+            <div className='flex justify-center pt-10 mx-auto h-scrren w-screen'>
+                <div className="bg-gray-100 w-4/6">
+                    <ul>
+                        {devices.map((item, index) => {
+                            return <DeviceCard device={item} key={index} />
+                        })}
+                    </ul>
+                </div>
+                <div className="bg-gray-200 w-2/6">
                     <FileSystemNavigator tree={tree} />
-                </ul>
+                </div>
             </div>
         </div>
     );
