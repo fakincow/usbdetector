20437b71a4f643ec74ab999136be1eb532dd87b6
fakincow <zabaroth@gmail.com>
2022-11-28 16:23:08 +0200

    dd

--- mcetask/client/ReactClient/wslistener/src/components/Navigation.tsx
+++ mcetask/client/ReactClient/wslistener/src/components/Navigation.tsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export function Navigation() {
     return (
         <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
-            <h3>MCE Systems Device Tree Exercise</h3>
+            <h3 className="text-lg font-bold">MCE Systems Device Tree Exercise</h3>
             <span>
-                <Link to="/" className="mr-2">MAIN VIEW</Link>
+                <Link to="/" className="mr-2 font-bold">MAIN VIEW</Link>
             </span>
         </nav>
     )
